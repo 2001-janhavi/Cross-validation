27911cde9fdaa429598407a482e60553ceffed0d
2001-janhavi <103043627+2001-janhavi@users.noreply.github.com>
2024-06-30 20:03:33 +0530

    Add files via upload

---
+++ Cross Validation.R
@@ -0,0 +1,24 @@
+library(ISLR)
+head(Auto)
+plot(horsepower,mpg)
+
+set.seed (1) 
+# randomly select 196 units 
+# from the original data set. 
+train <-sample(392,196)
+
+lm.fit <-lm(mpg~horsepower , data = Auto, subset=train)
+summary(lm.fit)
+
+attach(Auto) 
+mean((mpg-predict(lm.fit,Auto))[-train]^2)
+
+
+lm.fit2=lm(mpg~poly(horsepower ,2),data=Auto, subset=train) 
+mean((mpg-predict(lm.fit2,Auto))[-train]^2)
+
+lm.fit3=lm(mpg~poly(horsepower ,3),data=Auto, subset=train) 
+mean((mpg-predict(lm.fit3,Auto))[-train]^2)
+
+
+
